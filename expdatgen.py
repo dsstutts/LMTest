e9ed9b5da9bbe7d3e4d288c066f864a3c826c47e
Dan Stutts <stutts@mst.edu>
2017-10-06 09:22:41 -0500

    Fixed writedat formatting, added .gitignore, and added support for both Python 3 and Python 2.

--- expdatgen.py
+++ expdatgen.py
@@ -11,6 +11,8 @@ Based on expdatgen.m by D. S. Stutts: 2-4-2014
 from pylab import *
 import matplotlib.pyplot as plt
 import itertools# itteration tools for writedat() defined below
+import sys
+cur_version = sys.version_info
 
 # Create some empty lists to work with:
 x = []
@@ -26,12 +28,14 @@ def F(x):
        
 
 # Function to write formatted tab-delimited x,y data
-# from: http://rosettacode.org/wiki/Write_float_arrays_to_a_text_file#Python:
-def writedat(filename, x, y, xprecision=6, yprecision=6):
-    with open(filename,'w') as f:
-        for a, b in itertools.izip(x, y):
-            print >> f, "%.*g\t%.*g" % (xprecision, a, yprecision, b)
-            close(filename)
+# From: https://rosettacode.org/wiki/Write_float_arrays_to_a_text_file#Python
+def writedat(filename, x, y, xprecision=8, yprecision=8):
+   with open(filename,'w') as f:
+       for a, b in zip(x, y):
+            print("%.*g\t%.*g" % (xprecision, a, yprecision, b), file=f)
+            # or, using the new-style formatting:
+            # print("{1:.{0}g}\t{3:.{2}g}".format(xprecision, 
+            # a, yprecision, b), file=f)  
 
 # Create array of function values for each x:
 y = F(x)
@@ -44,16 +48,22 @@ infile = "data.txt"
 try: 
     data = open(infile, "r")# Get array out of input file
 except:
-    print "Cannot find input file; Please try again."
+    print ("Cannot find input file; Please try again.")
     sys.exit(0)
 
 # Close the input file:
 close(infile)
 # Parse the x,y pairs into two lists:
-for line in data: # Separate the x,y data by splitting at the white space (tab).
+if cur_version[0]==3:# This is necesary due to the change in the type
+    # returned by map in Python3.x.x
+    for line in data: # Separate the x,y data by splitting at the white space (tab).
+        xx.append(list(map(float, (line).split()))[0])
+        yy.append(list(map(float, (line).split()))[1])
+
+if cur_version[0]==2:# This is necesary due to the change in the type
+    for line in data: # Separate the x,y data by splitting at the white space (tab).
         xx.append(map(float, (line).split())[0])
         yy.append(map(float, (line).split())[1])
-
 # Recast the data lists into numpy arrays for plotting:     
 xx = array(xx)
 yy = array(yy) 
